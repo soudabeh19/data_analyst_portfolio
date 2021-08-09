0890775279634fc56d95cc49950ef099d9b3d47d
soudabeh19 <s_arghi@encs.concordia.ca>
2021-08-09 18:48:39 -0400

    Query: MAX, Cast

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -14,3 +14,41 @@ select Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100
 from CovidAnalysisProject..CovidDeaths
 where location like '%states%'
 order by 1,2
+
+-- How much worse is it? Total cases vs population!!
+-- Percentage of population who have Covid (daily infection rate compared to population)
+select Location, date,  population, total_cases, (total_cases/population)*100 as PctgPopulationInfected
+from CovidAnalysisProject..CovidDeaths
+where location like '%states%'
+order by 1,2
+
+-- Country with the highist infection rate compare to its population (order by the highest infected population)
+select Location, population, MAX(total_cases) as highestInfectionReported, MAX(total_cases/population)*100 as PctgPopulationInfected
+from CovidAnalysisProject..CovidDeaths
+group by Location, population
+order by PctgPopulationInfected desc
+
+-- Country with the highist death number compare to its population (order by the highest death rate population)
+-- Note: 1. total_deaths is a nvarchar type and it needs to convert to int
+--		 2. some records have NULL value in their "continent" columns therefore their location represents by their continent name
+select Location, MAX(cast(total_deaths as int)) as TotalDeathNum 
+from CovidAnalysisProject..CovidDeaths
+where continent is not null
+group by Location
+order by TotalDeathNum desc
+
+
+-- (Correct One) Continent with the highist death number compare to its population (order by the highest death rate population)
+select location, MAX(cast(total_deaths as int)) as TotalDeathNum 
+from CovidAnalysisProject..CovidDeaths
+where continent is null
+group by location
+order by TotalDeathNum desc
+
+-- (Just for being the same as ALEX project however the above query is accurate not the bellow one- for the sake of working with Tableau) Continent with the highist death number compare to its population (order by the highest death rate population)
+select continent, MAX(cast(total_deaths as int)) as TotalDeathNum 
+from CovidAnalysisProject..CovidDeaths
+where continent is not null
+group by continent
+order by TotalDeathNum desc
+
